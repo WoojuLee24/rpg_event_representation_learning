9147e82943d9722b44976f835a327d5991a935e3
woojulee24 <bigtree1234333@gmail.com>
2021-07-29 06:46:12 +0000

    NCAR dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,6 +1,9 @@
 import numpy as np
 from os import listdir
 from os.path import join
+import torch
+import loris
+from numpy.lib.recfunctions import structured_to_unstructured
 
 
 def random_shift_events(events, max_shift=20, resolution=(180, 240)):
@@ -47,6 +50,46 @@ class NCaltech101:
         label = self.labels[idx]
         f = self.files[idx]
         events = np.load(f).astype(np.float32)
+        if self.augmentation:
+            events = random_shift_events(events)
+            events = random_flip_events_along_x(events)
+        # normalizing time stamps
+        events[:, 2] = events[:, 2] / events[:, 2].max(axis=0)
+        return events, label
+
+
+class NCARS(torch.utils.data.Dataset):
+    def __init__(self, root, augmentation=False):
+        self.classes = listdir(root)
+
+        self.files = []
+        self.labels = []
+
+        self.augmentation = augmentation
+
+        for i, c in enumerate(self.classes):
+            new_files = [join(root, c, f) for f in listdir(join(root, c))]
+            self.files += new_files
+            self.labels += [i] * len(new_files)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        """
+        returns events and label, loading events from aedat
+        :param idx:
+        :return: x,y,t,p,  label
+        """
+        label = self.labels[idx]
+        f = self.files[idx]
+        # events = np.load(f).astype(np.float32)
+        with open(f, 'r') as f:
+            d = f.readlines()
+        events = loris.read_file(f)
+        events = np.array(structured_to_unstructured(events, dtype=np.float32))
+        # swap [t,x,y,p] to [x,y,t,p]
+        events = events[:, [1, 2, 0, 3]]
         if self.augmentation:
             events = random_shift_events(events)
             events = random_flip_events_along_x(events)
